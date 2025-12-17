b1019db56c8a934893b2a6504ed6bbf87b264286
pnkwra <pinkan.wa04@gmail.com>
2025-12-17 17:17:55 +0700

    tugas

--- tugasp11.piramid.py
+++ tugasp11.piramid.py
@@ -1,4 +1,5 @@
 character = input("Masukkan karakter: ")
+tinggi = int(input("Masukkan jumlah tinngi: "))
 
-for i in range(1, 20):
-    print(character.center(80))
+for i in range(1, tinggi + 1):
+    print(((character + " ") * i).center(80))
